32feb1429a2bff52e88e0b56fdf0b32a35d06e9b
TomasR <tomitotorios@gmail.com>
2024-09-18 22:58:47 -0300

    Se agrega la implementación completa del ejercicio 2 de la guía 2

--- firmware/projects/Ejercicio2Guia2/main/Ejercicio2Guia2.c
+++ firmware/projects/Ejercicio2Guia2/main/Ejercicio2Guia2.c
@@ -32,14 +32,248 @@
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
-/*==================[macros and definitions]=================================*/
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "hc_sr04.h"
+#include "lcditse0803.h"
+#include "switch.h"
+#include "timer_mcu.h"
 
+/* ==================[macros and definitions]=================================*/
+/**
+ * @def DELAY
+ * @brief Tiempo de refresco de medición, encendido de LEDs y muestra en pantalla LCD en milisegundos.
+ */
+#define TASK_DELAY 1000
+
+/**
+ * @def SWITCHES_DELAY
+ * @brief Tiempo de delay para el control de lectura de los switches en milisegundos.
+ */
+#define SWITCHES_DELAY 100
+
+/**
+ * @brief Distancia medida por el sensor en centímetros.
+ */
+uint16_t distancia=0;
+
+/**
+ * @brief Variable booleana que indica si se realiza la medición y muestra de distancia.
+ */
+bool global_on = false;
+
+/**
+ * @brief Variable booleana que indica si se mantiene el último valor medido.
+ */
+bool global_hold = false;
 /*==================[internal data definition]===============================*/
+/**
+ * @fn static void TaskTeclas(void *pvParameter)
+ * @brief Función encargada de leer el estado de los switches y actualizar las variables on y hold.
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ */
+TaskHandle_t TaskTeclas_task_handle = NULL;
+
+/**
+ * @fn static void MedirTask(void *pvParameter)
+ * @brief Función encargada de, en caso de estar encendido el dispositivo, medir la distancia con el sensor.
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ */
+TaskHandle_t MedirTask_task_handle = NULL;
+
+
+/**
+ * @fn static void MostrarTask(void *pvParameter)
+ * @brief Función que controla los LEDs y muestra el valor de la distancia medida en la pantalla LCD.
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ */
+TaskHandle_t MostrarTask_task_handle = NULL;
 
 /*==================[internal functions declaration]=========================*/
+/**
+ * @fn static void TaskTeclas(void *pvParameter)
+ * @brief Tarea encargada de leer el estado de los switches y cambiar las variables global_on y global_hold.
+ * Las teclas están asignadas de la siguiente manera:
+ * - TEC1: ON/OFF del dispositivo de medición.
+ * - TEC2: Activa o desactiva el modo "hold", que dejá mostrando el ultimo valor medido en la pantalla.
+ * 
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ * @return
+ */
+static void TaskTeclas(void *pvParameter);
+
+/**
+ * @fn static void MedirTask(void *pvParameter)
+ * @brief Tarea encargada de medir la distancia por medio del sensor.
+ * 
+ * Si la  variable global_on es verdadera, es decir, si el dispositivo está encendido
+ * se realiza la medición.
+ * 
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ * @return
+ */
+static void MedirTask(void *pvParameter);
+
+/**
+ * @fn static void MostrarTask(void *pvParameter)
+ * @brief Tarea encargada de controlar los LEDs y la pantalla LCD según la distancia medida.
+ * 
+ * Según la distancia medida en la tarea  MedirTask, se encienden los LED correspondientes:
+ * 
+ * - Menos de 10 cm: Apaga todos los LEDs.
+ * - Entre 10 y 20 cm: Enciende el LED_1.
+ * - Entre 20 y 30 cm: Enciende el LED_1 y el LED_2.
+ * - Más de 30 cm: Enciende el LED_1, LED_2 y LED_3.
+ * 
+ * Junto con esto, si no se está en modo HOLD, muestra la distancia medida en la pantalla LCD.
+ * 
+ * @param pvParameter Parámetro de la tarea (no utilizado).
+ * @return
+ */
+static void MostrarTask(void *pvParameter);
 
 /*==================[external functions definition]==========================*/
-void app_main(void){
-	printf("Hello world!\n");
+/**
+ * @brief Función invocada en la interrupción del timer A
+ */
+void FuncTimerA(void* param){
+    vTaskNotifyGiveFromISR(MedirTask_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_1 */
 }
-/*==================[end of file]============================================*/
+
+/**
+ * @brief Función invocada en la interrupción del timer B
+ */
+void FuncTimerB(void* param){
+    vTaskNotifyGiveFromISR(MostrarTask_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_2 */
+}
+/**
+ * @brief Función invocada en la interrupción del timer C
+ */
+void FuncTimerC(void* param){
+    vTaskNotifyGiveFromISR(TaskTeclas_task_handle, pdFALSE);    /* Envía una notificación a la tarea asociada al LED_2 */
+}
+
+static void MedirTask(void  *pvParameter){
+
+	while (true)
+	{	
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);  
+		if(global_on==true)
+		distancia = HcSr04ReadDistanceInCentimeters();
+	}
+	
+}
+
+static void MostrarTask(void *pvParameter){
+    while(true){
+		//Checkeo si la  tarea de medición está activa
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);  
+
+		if(global_on == true)
+		{
+			// Enciendo LEDs según la distancia
+			if(distancia < 10){
+				LedsOffAll();
+			}
+			else if (distancia >= 10 && distancia < 20){
+				LedOn(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
+			}
+			else if(distancia >= 20 && distancia < 30){
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOff(LED_3);
+			}
+			else if(distancia >= 30){
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOn(LED_3);
+			}
+
+			// Si el modo hold no está activado, muestro distancia en la pantalla LCD
+			if(global_hold == false){
+				LcdItsE0803Write(distancia);
+			}
+		}
+		else
+		{
+			// Apagar LEDs y pantalla LCD si on está apagado
+			LedOff(LED_1);
+			LedOff(LED_2);
+			LedOff(LED_3);
+			LcdItsE0803Off();
+		}
+    }
+}
+
+static void TaskTeclas(void *pvParameter){
+    while(true){
+
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);  
+
+		uint8_t switches = SwitchesRead();
+		switch(switches)
+		{
+			case SWITCH_1:
+				global_on = !global_on;
+			break;
+			
+			case SWITCH_2:
+				global_hold = !global_hold;
+			break;
+
+			case SWITCH_1 | SWITCH_2:
+				global_on =! global_on;
+				global_hold =! global_hold;
+			break;
+		}
+    }
+}
+/*==================[external functions definition]==========================*/
+/**
+ * @brief Función principal de la aplicación. Inicializa el hardware y crea las tareas para medir distancia,
+ *  mostrarla por medio de la pantalla y los LEDs y leer las teclas.
+ */
+void app_main(void){
+	//inicialización del hardware
+	LedsInit();
+	HcSr04Init(GPIO_3, GPIO_2);
+	LcdItsE0803Init();
+	SwitchesInit();
+	//inicialización de los timers
+	    timer_config_t timer_MedirTask = {
+        .timer = TIMER_A,
+        .period = TASK_DELAY,
+        .func_p = FuncTimerA,
+        .param_p = NULL
+    };
+	TimerInit(&timer_MedirTask);
+		timer_config_t timer_MostrarTask = {
+        .timer = TIMER_B,
+        .period = TASK_DELAY,
+        .func_p = FuncTimerB,
+        .param_p = NULL
+    };
+	TimerInit(&timer_MostrarTask);
+		timer_config_t timer_TaskTeclas = {
+        .timer = TIMER_C,
+        .period = SWITCHES_DELAY,
+        .func_p = FuncTimerC,
+        .param_p = NULL
+    };
+	TimerInit(&timer_TaskTeclas);
+
+	//creación de tareas
+	xTaskCreate(&MostrarTask, "MostrarTask", 2048, NULL, 4, &MostrarTask_task_handle);
+	xTaskCreate(&MedirTask, "MedirTask", 2048, NULL, 4, &MedirTask_task_handle);
+    xTaskCreate(&TaskTeclas, "TaskTeclas",2048, NULL, 5, &TaskTeclas_task_handle);
+	//inicialización del conteo de los timers
+	TimerStart(timer_MedirTask.timer);
+	TimerStart(timer_MostrarTask.timer);
+	TimerStart(timer_TaskTeclas.timer);
+	}
+
+/*=================[end of file]============================================*/
